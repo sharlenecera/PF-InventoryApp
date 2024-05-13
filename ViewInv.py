b0febd3a6b765862d3a81f863be855bef0ca9cc2
Sharlene Cera <cera.sharlene@gmail.com>
2024-05-13 22:27:38 +0100

    Fixed ValueError as I did not iterate through the dictionary.items()

--- ViewInv.py
+++ ViewInv.py
@@ -7,10 +7,13 @@ def view_inventory():
         print('\033[91mInventory is empty.\033[0m')
         return
 
-    # Display current inventory
-    print('Current inventory:')
+    
     try:
-        for item, quantity in inventory:
+        # Display current inventory
+        print('Current inventory:')
+        for item, quantity in inventory.items():
             print(f'{item}: {quantity}')
     except TypeError as error:
         print(f'{error}: Inventory is empty.')
+    except Exception as e:
+        print(f'Error: {e}')
