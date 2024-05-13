c1df007506200e169fba8524358873edd90276c3
Sharlene Cera <cera.sharlene@gmail.com>
2024-05-14 00:45:35 +0100

    Changed ViewInv.py for the dict to list of dict switch.

--- ViewInv.py
+++ ViewInv.py
@@ -3,17 +3,15 @@ import Inventory
 def view_inventory():
     # Load existing inventory
     inventory = Inventory.import_inventory('inventory.csv')
-    if inventory is None:
+    if not inventory:
         print('\033[91mInventory is empty.\033[0m')
         return
 
-    
     try:
         # Display current inventory
-        print('Current inventory:')
-        for item, quantity in inventory.items():
-            print(f'{item}: {quantity}')
-    except TypeError as error:
-        print(f'{error}: Inventory is empty.')
+        print('\033[93mCurrent inventory:\n')
+        for item in inventory:
+            print(f'Name: {item['name']} \nQuantity: {item['quantity']} \nPrice: {item['price']} \n')
+        print('\033[0m')
     except Exception as e:
         print(f'Error: {e}')
