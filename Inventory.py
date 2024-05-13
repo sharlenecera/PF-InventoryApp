0200ef34462a6f6683879305dab218c6772c2ecb
Sharlene Cera <cera.sharlene@gmail.com>
2024-05-13 21:43:38 +0100

    started nested list to dictionary migration. Changed the import and export functions in Inventory.py to accomodate the new dictionary format

--- Inventory.py
+++ Inventory.py
@@ -1,20 +1,46 @@
 import csv
 
 def import_inventory(file_name):
+
+    # Read the data from the CSV file
     try:
-        with open(file_name, 'r') as file:
-            reader = csv.reader(file)
-            inventory = list(reader)
-        return inventory
+        with open(file_name, 'r', newline='') as file:
+            reader = csv.DictReader(file)
+            # initialise empty dictionary to store the row
+            # the first row is the inventory to return
+            for row in reader:
+                print(f'row: {row}')
+                return row
+        print('Inventory imported successfully.')
     except FileNotFoundError:
         print('File not found. Creating now...')
-        return []
+        return dict()
     
-def export_inventory(file_name, inventory):
+def export_inventory(output_file_name, inventory):
+    # Write the data to the CSV file
     try:
-        with open(file_name, 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerows(inventory)
+        with open(output_file_name, 'w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=list(inventory.keys()))
+            # Write the header row (dictionary keys)
+            writer.writeheader()
+            # Write the dictionary row to the csv file
+            writer.writerow(inventory)
         print('Inventory exported successfully.')
     except Exception as e:
         print(f'Error exporting inventory: {e}')
+
+'''
+How the inventory looks in two different formats
+
+Dictionary:
+{
+    iPhone: 100
+    iPad: 20
+    mac: 30
+}
+
+CSV:
+iPhone, iPad, mac
+100, 20, 30
+
+'''
