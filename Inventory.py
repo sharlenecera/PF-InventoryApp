a745cb29e6ee20ce77ba87fa0ae5e931a2c57b6e
Sharlene Cera <cera.sharlene@gmail.com>
2024-05-14 00:27:45 +0100

    Changing Inventory.py due to change from dictionary to list of dictionary inventory.

--- Inventory.py
+++ Inventory.py
@@ -4,38 +4,81 @@ def import_inventory(file_name):
 
     # Read the data from the CSV file
     try:
+        # Read the data from the CSV file
         with open(file_name, 'r', newline='') as file:
+            inventory = []
             reader = csv.DictReader(file)
-            # initialise empty dictionary to store the row
-            # the first row is the inventory to return
             for row in reader:
-                print(f'row: {row}')
-                return row
-        print('Inventory \033[92mimported\033[0m successfully.')
+                inventory.append(row)
+        return inventory
     except FileNotFoundError:
         print('File not found. Creating now...')
         # creating empty file
         with open(file_name, 'w') as file: 
             print("Empty file created Successfully")
-            pass 
-        return dict()
+        return []
     
 def export_inventory(output_file_name, inventory):
     # Write the data to the CSV file
     try:
+
+        fieldnames = ['name', 'price', 'quantity']
+
         with open(output_file_name, 'w', newline='') as file:
-            writer = csv.DictWriter(file, fieldnames=list(inventory.keys()))
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
             # Write the header row (dictionary keys)
             writer.writeheader()
             # Write the dictionary row to the csv file
-            writer.writerow(inventory)
+            writer.writerows(inventory)
         print('Inventory exported successfully.')
     except Exception as e:
-        print(f'Error exporting inventory: {e}')
+        print(f'\033[91mError exporting inventory: {e}\033[0m')
 
 '''
 How the inventory looks in two different formats
 
+NEW ------------------------
+
+Nested Dictionary:
+{
+    'iPhone': {
+        'Price': '999',
+        'Quantity': '200'
+    },
+    'iPad': {
+        'Price': '500',
+        'Quantity': '100'
+    },
+    'mac': {
+        'Price': '2000',
+        'Quantity': '50'
+    }
+}
+
+List of Dictionaries:
+[
+    {
+        'Name': 'iPhone'
+        'Price': '999',
+        'Quantity': '200'
+    },
+    {
+        'Name': 'iPad'
+        'Price': '500',
+        'Quantity': '100'
+    },
+    {
+        'Name': 'mac'
+        'Price': '2000',
+        'Quantity': '50'
+    }
+]
+
+CSV:
+iPhone,iPad,mac
+"{'Price': '999', 'Quantity': '200'}","{'Price': '500', 'Quantity': '100'}","{'Price': '2000', 'Quantity': '50'}"
+
+OLD ------------------------
 Dictionary:
 {
     iPhone: 100
