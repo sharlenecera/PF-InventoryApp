fe44bec7d1107b71860b686c51b3bcf7c20dd908
Sharlene Cera <cera.sharlene@gmail.com>
2024-05-14 01:24:13 +0100

    Changed RemInv.py to accommodate the dict to list of dict inventory switch.

--- RemInv.py
+++ RemInv.py
@@ -1,5 +1,5 @@
 import Inventory
-from EditInv import get_position_input, validate_position_input, get_item_by_index
+from EditInv import get_position_input, validate_position_input
 
 def remove_item():
     # Load existing inventory
@@ -8,13 +8,12 @@ def remove_item():
     # Display current inventory
     print('Current inventory:')
     for i, item in enumerate(inventory):
-        print(f'{i+1}, {item}: {inventory[item]}')
+        print(f'{i+1} -> {item['name']}')
 
     # Get input for item to remove
     index = validate_position_input(get_position_input(), len(inventory)) - 1
 
     # Remove item from inventory
-    item_to_delete = get_item_by_index(inventory, index)
-    del inventory[item_to_delete]
+    del inventory[index]
     Inventory.export_inventory('inventory.csv', inventory)
     print('\033[92mItem removed successfully.\033[0m')
